f051bdabc6086fac5bc093512abdaa4160bae1d7
米鸿坤 <1045262335@qq.com>
2021-12-17 10:47:08 +0800

    chero: tidy

---
+++ src/lib1.lua
@@ -0,0 +1,11 @@
+function norm(x,y)
+    return math.sqrt(x^2+y^2)
+end
+
+function twice(x) 
+    return 2.0 * x
+end
+--[[
+我是注释
+]]
+
---
+++ src/type.lua
@@ -0,0 +1,13 @@
+function p(x)
+    print(type(x))
+end
+
+p(nil)
+p(true)
+p(10.4*3)
+p("Hello world")
+p(io.stdin)
+p(print)
+p(type)
+p({})
+p(type(X))
